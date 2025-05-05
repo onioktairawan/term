2b729fc1a8a129700f1d47b1ac0077e815585ca7
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-05-05 09:31:31 +0700

    Create daftarproduk.py

---
+++ daftarproduk.py
@@ -0,0 +1,24 @@
+# daftarproduk.py
+
+produk_list = [
+    {
+        "nama": "Premium 1 Bulan",
+        "durasi": 1,
+        "harga": 15000
+    },
+    {
+        "nama": "Premium 3 Bulan",
+        "durasi": 3,
+        "harga": 40000
+    },
+    {
+        "nama": "Premium 6 Bulan",
+        "durasi": 6,
+        "harga": 75000
+    },
+    {
+        "nama": "Premium 12 Bulan",
+        "durasi": 12,
+        "harga": 140000
+    }
+]
